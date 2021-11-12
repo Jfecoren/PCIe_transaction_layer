aa3f5b5026446359461c1324137b59a5aa60b205
locaampo010 <luisdocampo@gmail.com>
2021-11-12 16:24:30 -0600

    Adding TCL files for Referees

---
+++ src/TCL/referee_1.tcl
@@ -0,0 +1,9 @@
+read_verilog referee_1.v
+hierarchy -check -top referee_1
+proc; opt; fsm; opt; memory; opt
+techmap; opt
+dfflibmap -liberty ../../include/cmos_cells.lib
+abc -liberty ../../include/cmos_cells.lib
+clean
+show  -lib ../../include/cmos_cells.v referee_1
+write_verilog referee_1_est.v
---
+++ src/TCL/referee_2.tcl
@@ -0,0 +1,9 @@
+read_verilog referee_2.v
+hierarchy -check -top referee_2
+proc; opt; fsm; opt; memory; opt
+techmap; opt
+dfflibmap -liberty ../../include/cmos_cells.lib
+abc -liberty ../../include/cmos_cells.lib
+clean
+show  -lib ../../include/cmos_cells.v referee_2
+write_verilog referee_2_est.v
