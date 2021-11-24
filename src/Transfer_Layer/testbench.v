0946ecd6ed3a8d0e443d7954d509207846c5b5f1
zetavB <jordi.louzao@gmail.com>
2021-11-23 20:24:17 -0800

    Se agrega sintesis

--- src/Transfer_Layer/testbench.v
+++ src/Transfer_Layer/testbench.v
@@ -1,12 +1,13 @@
 `timescale 	1ns	/ 100ps
 `include "tcl.v"
+`include "tcl_synth.v"
 `include "probador.v"
 
 module testbench();
-   wire clk, req, reset, init, popOutP0, popOutP1, popOutP2, popOutP3, pushIn, counterValid;
+   wire clk, req, reset, init, popOutP0, popOutP1, popOutP2, popOutP3, pushIn, counterValid, counterValid_synth;
    wire [2:0] Umbral_bajo, Umbral_alto, idx;
-   wire [4:0] counterOut;
-   wire [11:0] dataOutputP0, dataOutputP1, dataOutputP2, dataOutputP3, dataInputFIFO;
+   wire [4:0] counterOut, counterOut_synth;
+   wire [11:0] dataOutputP0, dataOutputP1, dataOutputP2, dataOutputP3, dataOutputP0_synth, dataOutputP1_synth, dataOutputP2_synth, dataOutputP3_synth, dataInputFIFO;
    
    probador tester(/*AUTOINST*/
 		   // Outputs
@@ -53,5 +54,28 @@ module testbench();
 		     .Umbral_alto	(Umbral_alto[2:0]),
 		     .idx		(idx[2:0]),
 		     .dataInputFIFO	(dataInputFIFO[11:0]));
+   tcl_synth transferLayer_synth(/*AUTOINST*/
+				 // Outputs
+				 .counterOut_synth	(counterOut_synth[4:0]),
+				 .counterValid_synth	(counterValid_synth),
+				 .dataOutputP0_synth	(dataOutputP0_synth[11:0]),
+				 .dataOutputP1_synth	(dataOutputP1_synth[11:0]),
+				 .dataOutputP2_synth	(dataOutputP2_synth[11:0]),
+				 .dataOutputP3_synth	(dataOutputP3_synth[11:0]),
+				 // Inputs
+				 .Umbral_alto		(Umbral_alto[2:0]),
+				 .Umbral_bajo		(Umbral_bajo[2:0]),
+				 .clk			(clk),
+				 .dataInputFIFO		(dataInputFIFO[11:0]),
+				 .idx			(idx[2:0]),
+				 .init			(init),
+				 .popOutP0		(popOutP0),
+				 .popOutP1		(popOutP1),
+				 .popOutP2		(popOutP2),
+				 .popOutP3		(popOutP3),
+				 .pushIn		(pushIn),
+				 .req			(req),
+				 .reset			(reset));
+   
    
 endmodule // testbench
